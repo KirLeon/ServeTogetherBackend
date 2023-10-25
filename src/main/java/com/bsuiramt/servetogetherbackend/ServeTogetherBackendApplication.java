1de7f72fb3a4cf4b60898e630ee62d4f0e319424
KirLeon <kirleon.dev@gmail.com>
2023-10-25 12:57:36 +0300

    #1D Initial commit

---
+++ src/main/java/com/bsuiramt/servetogetherbackend/ServeTogetherBackendApplication.java
@@ -0,0 +1,13 @@
+package com.bsuiramt.servetogetherbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServeTogetherBackendApplication {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(ServeTogetherBackendApplication.class, args);
+	}
+	
+}
---
+++ src/test/java/com/bsuiramt/servetogetherbackend/ServeTogetherBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bsuiramt.servetogetherbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServeTogetherBackendApplicationTests {
+	
+	@Test
+	void contextLoads() {
+	}
+	
+}
